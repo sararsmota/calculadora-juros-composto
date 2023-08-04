c11f4475dad4da756b991664870985e8a68ca0b6
Sara Roseane <80297582+sararoseane@users.noreply.github.com>
2023-08-04 17:03:55 -0300

    feat: add function mothly application

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,30 @@
+//calcular os valores inseridos
 document.getElementById('calculate').addEventListener('click', function(){
     const value = document.getElementById('value').value;
-    const fee = (document.getElementById('fee').value) / 100;
+    const fee = (document.getElementById('fee').value);
     const time = document.getElementById('time').value;
+    const monthly = document.getElementById('monthly').value;
 
-    let total_invested = value;
-    // total_invested = "R$ " + Number(total_invested).toFixed(2).replace('.',',');
+    let fv = FV(monthly, fee, time, value);
+    let total = fv;
 
-    let total = value * (1 + fee)**time;
-
+    let total_invested = parseInt(value) + (monthly * time);
     let total_gain = total - total_invested;
-    // total_gain = "R$ " + Number(total_gain).toFixed(2).replace('.',',');
 
     document.getElementById('total_invested').innerHTML = formatter.format(total_invested);
     document.getElementById('total_gain').innerHTML =  formatter.format(total_gain);
     document.getElementById('total').innerHTML = formatter.format(total);
-
-    a = formatter.format(total_invested);
-    console.log(a)
-
 });
 
-  const formatter = new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-    minimumFractionDigits: 2,
-  });
+//função para formatar na moeda Real
+const formatter = new Intl.NumberFormat("pt-BR", {
+  style: "currency",
+  currency: "BRL",
+  minimumFractionDigits: 2,
+});
+
+// função de juros compostos com aportes mensais
+function FV(PMT, i, n, PV) {
+  var i = i / 100 //converter porcentagem
+  return ((PV * (Math.pow(1+i, n)))) + ((PMT * (Math.pow(1 + i, n) - 1))/i)
+}
